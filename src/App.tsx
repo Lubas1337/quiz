a5226347ef93109ac8a1f768c5a2f6ab31382964
abylay <abylailuba@gmail.com>
2025-03-05 21:48:52 +0500

    add os rk 1

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ const MINIMUM_QUESTIONS = 30;
 
 // Add available quiz files
 const AVAILABLE_QUIZZES = [
+    {id: 'os rk1', name: 'os rk1', file: '/os_rk1.docx'},
     {id: 'philosophy', name: 'Philosophy', file: '/pshyc.docx'},
     {id: 'Webka', name: 'Webka', file: '/webka2.docx'},
     {id: 'ix', name: 'uiux', file: '/ix.docx'},
